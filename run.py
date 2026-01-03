6db799bfe25df82393a1ea5662b08419650d509f
Sahil Khan <darkstar666n@gmail.com>
2026-01-04 02:14:36 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
-# run.py
-
 import importlib.util
 
 print("[*] Loader started")
 
 path = "./obf-Da77.py"
 
-spec = importlib.util.spec_from_file_location("x", path)
+spec = importlib.util.spec_from_file_location("obf_mod", path)
 mod = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(mod)
 
-print("[+] obf-Da77.py loaded via importlib")
+print("[+] obf-Da77.py loaded")
+
+# Agar aap file ke andar ka 'execute' function chalana chahte hain:
+if hasattr(mod, '_cube'):
+    print("[*] Executing _cube logic...")
+    mod._cube.execute(code=mod.__code__)
