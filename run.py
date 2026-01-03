32215f8743c27849318607d0196f55f62acdc42b
Sahil Khan <darkstar666n@gmail.com>
2026-01-04 02:00:29 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+# run.py
+
+print("[*] Loader started")
+
+try:
+    import obf_Da77
+    print("[+] obf_Da77 loaded successfully")
+except Exception as e:
+    print("[!] Error while loading obf_Da77:", e)
