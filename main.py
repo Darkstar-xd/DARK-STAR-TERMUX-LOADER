757feebcdfca10de94f4111e4ee92055aaa25e5f
Sahil Khan <darkstar666n@gmail.com>
2026-01-09 23:14:23 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import Sahil_Lundsari_all_encrypted
+import sys
+
+def start():
+    try:
+        Sahil_Lundsari_all_encrypted.run()
+    except Exception as e:
+        sys.exit()
+
+if __name__ == "__main__":
+    start()
